f4c0850ff6a7954ce964e896c41488936e99f044
Harveer Singh <142636952+Harveertooshiesty@users.noreply.github.com>
2024-04-03 15:42:21 +0000

    Wed, Apr 3, 2024, 8:42 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,5 +1,22 @@
 rulesBtn = document.getByElementById('rules-btn')
 rules = document.getByElementById('rules')
 closeBtn = document.getByElementById('close-btn')
+canvas = document.getByElementById('canvas')
+ctx = canvas.getContext('2d'
+)
+rulesBtn.addEventListener( 'click', () => {
+    rules.classList.add('show')
+})
+closeBtn.addEventListener( 'click;, () => {
+    rules.classList.remove('show')
+})
 
-rulesBtn.addEventListener
+ball = {
+    x: canvas-width/2,
+    y: canvas-height/2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+
+}
